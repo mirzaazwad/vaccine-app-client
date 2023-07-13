754b07276685d4f7de34ed6ffce9a0c42d75fec7
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2023-07-13 20:33:53 +0600

    Changes made

--- server/routes/vaccination.route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { vaccine_register, view_vaccines} = require("../controllers/vaccination.controller");
-
-router.post("/vaccine_register", vaccine_register);
-router.get("/view_vaccines/:nid", view_vaccines);
-
-module.exports = router;
