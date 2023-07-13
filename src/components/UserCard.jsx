cb521c321f3c6aef134ab109121b58ef60d78074
curryfizz <nawsheen141@gmail.com>
2023-07-14 04:26:48 +0600

    user info is fetched properly

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 
 const UserCard = () => {
   const [nid, setNid] = useState("");
+  const [userInfo, setUserInfo] = useState(null);
   useEffect(() => {
     if (localStorage.nid) {
       setNid(localStorage.nid);
+      fetchUserInfo();
     }
   }, [])
 
   const fetchUserInfo = async () => {
-    try {
 
+    const config = {
+      header: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    console.log(nid);
+    try {
+      const user = await axios.get(`https://vaccine-app-server-kilfewcikq-uc.a.run.app/api/user/get-user/${nid}`, config)
+      
+      console.log(user.data);
+
+
+      if(user.data.success){
+        setUserInfo(user.data.user);
+      }
     } catch (error) {
 
     }
@@ -27,20 +45,17 @@ const UserCard = () => {
         <div>
           <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
             <p className="text-gray-600">Full name</p>
-            <p>Jane Doe</p>
+            <p>{userInfo?userInfo.name: "John doe"}</p>
           </div>
           <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
             <p className="text-gray-600">NID</p>
-            <p>Product Manager</p>
+            <p>{userInfo?userInfo.n_id: "Not Found!"}</p>
           </div>
           <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
-            <p className="text-gray-600">Vaccine Dose Completed</p>
-            <p>Janedoe@gmail.com</p>
+            <p className="text-gray-600">Address</p>
+            <p>{userInfo?userInfo.address:"Not Found!"}</p>
           </div>
-          <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
-            <p className="text-gray-600">Next Vaccine Due</p>
-            <p>$ 12000</p>
-          </div>
+          
         </div>
       </div>
     </div>
