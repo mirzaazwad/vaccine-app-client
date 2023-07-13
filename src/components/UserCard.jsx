4c744433a55d2b32c20cb02714fec409b05c26b2
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2023-07-14 05:24:41 +0600

    loader

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Loader from "./Loader";
 
 const UserCard = () => {
   const [nid, setNid] = useState("");
@@ -9,7 +10,7 @@ const UserCard = () => {
       setNid(localStorage.nid);
       fetchUserInfo();
     }
-  }, []);
+  }, [nid]);
 
   const fetchUserInfo = async () => {
     console.log(nid);
@@ -25,37 +26,40 @@ const UserCard = () => {
         config
       );
 
-      console.log(user.data);
-
       if (user.data.success) {
         setUserInfo(user.data.user);
       }
     } catch (error) {}
   };
 
-  return (
-    <div className="min-h-screen py-3 flex items-center justify-center px-4 bg-indigo-100">
-      <div className="max-w-4xl  bg-white w-full rounded-xl shadow-xl  border border-indigo-300">
-        <div className="p-4 border-b">
-          <h2 className="text-2xl ">User Information</h2>
-        </div>
-        <div>
-          <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
-            <p className="text-gray-600">Full name</p>
-            <p>{userInfo ? userInfo.name : "John doe"}</p>
+  if(userInfo){
+    return (
+      <div className="min-h-screen py-3 flex items-center justify-center px-4 bg-indigo-100">
+        <div className="max-w-4xl  bg-white w-full rounded-xl shadow-xl  border border-indigo-300">
+          <div className="p-4 border-b">
+            <h2 className="text-2xl ">User Information</h2>
           </div>
-          <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
-            <p className="text-gray-600">NID</p>
-            <p>{nid}</p>
-          </div>
-          <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
-            <p className="text-gray-600">Address</p>
-            <p>{userInfo ? userInfo.address : "Not Found!"}</p>
+          <div>
+            <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
+              <p className="text-gray-600">Full name</p>
+              <p>{userInfo ? userInfo.name : "John doe"}</p>
+            </div>
+            <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
+              <p className="text-gray-600">NID</p>
+              <p>{nid}</p>
+            </div>
+            <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-4 border-b">
+              <p className="text-gray-600">Address</p>
+              <p>{userInfo ? userInfo.address : "Not Found!"}</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+  else{
+    return (<Loader/>)
+  }
 };
 
 export default UserCard;
